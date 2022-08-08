fd09b0c96b778371df8f743fe1f2f8d310055936
Paula2208 <pguzmanz@unal.edu.co>
2022-08-08 12:03:18 -0500

    mmm

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
+let sequelize;
 sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
